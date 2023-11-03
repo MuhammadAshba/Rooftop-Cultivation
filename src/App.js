71beca5e9437510b1caaa886016c946147524451
Majid <majidali37406@gmail.com>
2023-11-03 20:08:03 +0500

    some change in app .js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 
-import Header from './components/Header';
-import Showcollection from './components/Showcollection';
-import Products from './components/Products';
-import NewArrival from './components/NewArrival';
-// import Userdashboard from './Pages/user/Userdashboard';
+// import Header from './components/Header';
+// import Showcollection from './components/Showcollection';
+// import Products from './components/Products';
+// import NewArrival from './components/NewArrival';
+import Userdashboard from './Pages/user/Userdashboard';
 
 
 
 function App() {
   return ( 
     <div className="App">
-      {/* <Userdashboard/> */}
-      <Header/>
+      <Userdashboard/>
+      {/* <Header/>
       <Showcollection/>
       <Products/>
-      <NewArrival/>
+      <NewArrival/> */}
 
     </div>
   );
